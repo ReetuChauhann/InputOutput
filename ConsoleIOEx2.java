8558cd042aa4dfe97588db2724f15750238c2861
ReetuChauhann <chauhanreetu00@gmail.com>
2022-04-17 16:32:16 +0530

    Scanner class

---
+++ ConsoleIOEx2.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class ConsoleIOEx2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // System.out.print("Enter a Number");
+        // int a = sc.nextInt();    //for enterning the number 
+        // System.out.println("Number is: " + a);
+
+        // System.out.print("Enter your name :");
+        // String a = sc.next();       //only take the first word of the string
+        // System.out.println("your name is : " + a);
+
+        // System.out.print("Write something about yourself!");
+        // String a = sc.nextLine();  //to take multi word in a String
+        // System.out.println("Your Answer :" + a);
+
+        System.out.print("Enter your 10th percentage! :");
+        float a = sc.nextFloat();
+        System.out.println("Your percentage is : " + a );
+
+        
+
+    }
+    
+}
